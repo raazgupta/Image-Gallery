7e52f77660b52661e02d3333b02e891b37cd800e
Raj Gupta <raazgupta@gmail.com>
2024-02-25 09:39:25 +0900

    Adding ability to save title, favorite and stars

---
+++ Image Gallery v2/ImageDetailsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ImageDetailsViewController.swift
+//  Image Gallery v2
+//
+//  Created by Raj Gupta on 2024/02/24.
+//  Copyright © 2024 SoulfulMachine. All rights reserved.
+//
+
+import Foundation
