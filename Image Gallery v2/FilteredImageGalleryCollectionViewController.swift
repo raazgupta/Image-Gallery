ecf5fa148fc8811967ece4a0456fed5e738a8b6d
Raj Gupta <raazgupta@gmail.com>
2024-03-09 09:05:56 +0900

    Added ability to search by name, stars, favorite

---
+++ Image Gallery v2/FilteredImageGalleryCollectionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FilteredImageGalleryCollectionViewController.swift
+//  Image Gallery v2
+//
+//  Created by Raj Gupta on 2024/03/04.
+//  Copyright © 2024 SoulfulMachine. All rights reserved.
+//
+
+import Foundation
---
+++ Image Gallery v2/ImageSearchViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ImageSearchViewController.swift
+//  Image Gallery v2
+//
+//  Created by Raj Gupta on 2024/03/03.
+//  Copyright © 2024 SoulfulMachine. All rights reserved.
+//
+
+import Foundation
